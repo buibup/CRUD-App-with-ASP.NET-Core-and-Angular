29a8b5ae0d2aca60a810d58f597c7d457fb201e7
buibup <b5209194@gmail.com>
2018-05-20 23:03:20 +0700

    complete with backend

---
+++ CrudAngular/Data/WorkoutContext.cs
@@ -0,0 +1,39 @@
+﻿using Backend.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Backend.Data
+{
+    public class WorkoutContext : DbContext
+    {
+        public WorkoutContext()
+        {
+
+        }
+
+        public WorkoutContext(DbContextOptions<WorkoutContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                IConfigurationRoot configuration = new ConfigurationBuilder()
+                   .SetBasePath(Directory.GetCurrentDirectory())
+                   .AddJsonFile("appsettings.json")
+                   .Build();
+                var connectionString = configuration.GetConnectionString("WorkoutContext");
+                optionsBuilder.UseSqlServer(connectionString);
+            }
+        }
+
+        public DbSet<Workout> Workout { get; set; }
+    }
+}
